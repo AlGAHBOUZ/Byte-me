117810f77aaa3d9e05e215944d5746a8f40d77cd
A11Veles <105862931+A11Veles@users.noreply.github.com>
2022-12-28 22:34:53 +0200

    Create Latest_card_h.h

---
+++ Latest_card_h.h
@@ -0,0 +1,58 @@
+#ifndef HELLOSFML_CARD_H
+#define HELLOSFML_CARD_H
+
+#include <SFML/Graphics.hpp>
+#include<SFML/Window.hpp>
+#include<SFML/System.hpp>
+#include <bits/stdc++.h>
+
+//using namespace sf;
+//using namespace std;
+// boi?
+class Card{
+private:
+    int number;
+    bool hidden;
+    sf::Vector2f currPosition;
+    std::string path ,path_hidden, path_not_hidden;
+    enum symbol {
+        clover,
+        diamond,
+        hearts,
+        spades
+    } card_symb;
+    enum color {
+        red,
+        black,
+    } card_clr;
+    sf::RectangleShape card_sprite; // rectangle shape maybe replaced with sprite
+    sf::Texture card_texture;
+public:
+    // Contructors
+    Card();
+    Card(int num, std::string symb);
+    // Setters
+    void setNumber(int num);
+    void setHidden(bool h);
+    void setSymbolClr(std::string symb);
+    void setPosition(sf::Vector2f pos);
+    void setCurr();
+
+    // Getters
+    const int getNumber() ;
+    const int getHidden() ;
+    const std::string getColor() ;
+    const std::string getSymbol() ;
+    std::string getPath();
+    const sf::Vector2f getPosition();
+    sf::Vector2f getCurr();
+
+    // Functions
+    void drawTo(sf::RenderWindow &window);
+    void UpdatePosition(int i, int j);
+    sf::FloatRect getGlobalBounds();
+
+    bool isMouseOver(sf::RenderWindow &window);
+};
+
+#endif //HELLOSFML_CARD_H
