c3e810f93c15119a74696ddaff29f7117b602799
Ayman Talat <aymantalat2@gmail.com>
2022-12-31 23:31:27 +0200

    First commit w Git

---
+++ HelloSFML/Button.h
@@ -0,0 +1,36 @@
+#ifndef HelloSFML_BUTTON_H
+#define HelloSFML_BUTTON_H
+
+#include <SFML/Graphics.hpp>
+#include<SFML/Window.hpp>
+#include<SFML/System.hpp>
+#include <bits/stdc++.h>
+
+class Button{
+
+public:
+
+    Button(std::string t, sf::Vector2f size,  int charSize ,sf::Color bgColor, sf::Color textColor);
+
+    void setFont(sf::Font &font);
+
+    void setbackColor(sf:: Color color);
+
+    void setTextColor(sf:: Color color);
+
+
+    void setPosition(sf::Vector2f pos);
+
+    void drawTo(sf::RenderWindow &window);
+
+    bool isMouseOver(sf::RenderWindow &window);
+
+private:
+    sf::RectangleShape button;
+    sf::Text text;
+};
+
+
+
+
+#endif //HelloSFML_BUTTON_H
---
+++ HelloSFML/main.cpp
@@ -0,0 +1,33 @@
+
+#include "Game.h"
+
+int main() {
+    // Initializing the Solitiare game
+    Game game;
+
+    // Game loop
+    while (game.IsRunning()) {
+        // Events Loop
+        game.PollEvents();
+
+        // Comment the next line to turn off drag & drop feature (which is buggy)
+        //game.DragDrop();
+
+        // Check Winning Status
+        if (game.CheckWinStatus()) {
+            // Draw the winning frame
+            game.DrawWinFrame();
+        }
+        else {
+            // Draw
+            game.DrawFrame();
+        }
+
+        // Check Losing Status
+//        if (game.CheckLossStatus()) {
+//            // maybe another frame for losing (Loser hahaha!)
+//            std::cout << "Good For You! YOU ARE A LOSER HAHAHAHA.\n";
+//            break;
+//        }
+    }
+}
