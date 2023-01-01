5e03b40284b401b0474a91904520b06992b42c85
Ayman Talat <aymantalat2@gmail.com>
2023-01-01 14:32:53 +0200

    Updating TASKS_LIST file

---
+++ HelloSFML/cmake-build-debug/CMakeFiles/HelloSFML.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/EUI/Data Structures/Project/phase4/Byte-me/HelloSFML/Button.cpp" "CMakeFiles/HelloSFML.dir/Button.cpp.obj" "gcc" "CMakeFiles/HelloSFML.dir/Button.cpp.obj.d"
+  "D:/EUI/Data Structures/Project/phase4/Byte-me/HelloSFML/Card.cpp" "CMakeFiles/HelloSFML.dir/Card.cpp.obj" "gcc" "CMakeFiles/HelloSFML.dir/Card.cpp.obj.d"
+  "D:/EUI/Data Structures/Project/phase4/Byte-me/HelloSFML/Game.cpp" "CMakeFiles/HelloSFML.dir/Game.cpp.obj" "gcc" "CMakeFiles/HelloSFML.dir/Game.cpp.obj.d"
+  "D:/EUI/Data Structures/Project/phase4/Byte-me/HelloSFML/main.cpp" "CMakeFiles/HelloSFML.dir/main.cpp.obj" "gcc" "CMakeFiles/HelloSFML.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ HelloSFML/cmake-build-debug/CMakeFiles/HelloSFML.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloSFML.dir/Button.cpp.obj"
+  "CMakeFiles/HelloSFML.dir/Button.cpp.obj.d"
+  "CMakeFiles/HelloSFML.dir/Card.cpp.obj"
+  "CMakeFiles/HelloSFML.dir/Card.cpp.obj.d"
+  "CMakeFiles/HelloSFML.dir/Game.cpp.obj"
+  "CMakeFiles/HelloSFML.dir/Game.cpp.obj.d"
+  "CMakeFiles/HelloSFML.dir/main.cpp.obj"
+  "CMakeFiles/HelloSFML.dir/main.cpp.obj.d"
+  "HelloSFML.exe"
+  "HelloSFML.exe.manifest"
+  "HelloSFML.pdb"
+  "libHelloSFML.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HelloSFML.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
