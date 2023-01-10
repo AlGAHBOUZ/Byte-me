5d1d8dcdc8bbe75bd9ecf3aa95ad66af2ea3451a
Ayman Talat <92207032+Ayman-79@users.noreply.github.com>
2023-01-10 12:56:31 +0200

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/HelloSFML.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "RC"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_RC
+  "D:/EUI/Data Structures/Project/Solitaire Project (Byte Me Team)/Solitaire (Byte Me Team)/Icon_file.rc" "D:/EUI/Data Structures/Project/Solitaire Project (Byte Me Team)/Solitaire (Byte Me Team)/cmake-build-debug/CMakeFiles/HelloSFML.dir/Icon_file.rc.obj"
+  )
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_RC
+  "SFML_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_RC_TARGET_INCLUDE_PATH
+  "c:/SFML/include"
+  "C:/SFML/include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/EUI/Data Structures/Project/Solitaire Project (Byte Me Team)/Solitaire (Byte Me Team)/Button.cpp" "CMakeFiles/HelloSFML.dir/Button.cpp.obj" "gcc" "CMakeFiles/HelloSFML.dir/Button.cpp.obj.d"
+  "D:/EUI/Data Structures/Project/Solitaire Project (Byte Me Team)/Solitaire (Byte Me Team)/Card.cpp" "CMakeFiles/HelloSFML.dir/Card.cpp.obj" "gcc" "CMakeFiles/HelloSFML.dir/Card.cpp.obj.d"
+  "D:/EUI/Data Structures/Project/Solitaire Project (Byte Me Team)/Solitaire (Byte Me Team)/Game.cpp" "CMakeFiles/HelloSFML.dir/Game.cpp.obj" "gcc" "CMakeFiles/HelloSFML.dir/Game.cpp.obj.d"
+  "D:/EUI/Data Structures/Project/Solitaire Project (Byte Me Team)/Solitaire (Byte Me Team)/main.cpp" "CMakeFiles/HelloSFML.dir/main.cpp.obj" "gcc" "CMakeFiles/HelloSFML.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/HelloSFML.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloSFML.dir/Button.cpp.obj"
+  "CMakeFiles/HelloSFML.dir/Button.cpp.obj.d"
+  "CMakeFiles/HelloSFML.dir/Card.cpp.obj"
+  "CMakeFiles/HelloSFML.dir/Card.cpp.obj.d"
+  "CMakeFiles/HelloSFML.dir/Game.cpp.obj"
+  "CMakeFiles/HelloSFML.dir/Game.cpp.obj.d"
+  "CMakeFiles/HelloSFML.dir/Icon_file.rc.obj"
+  "CMakeFiles/HelloSFML.dir/main.cpp.obj"
+  "CMakeFiles/HelloSFML.dir/main.cpp.obj.d"
+  "HelloSFML.exe"
+  "HelloSFML.exe.manifest"
+  "HelloSFML.pdb"
+  "libHelloSFML.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX RC)
+  include(CMakeFiles/HelloSFML.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
